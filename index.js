7bfa6d241330178b369062349dcb0d51d692a430
George Wilkins <gwilki2@gmail.com>
2022-01-30 22:39:50 -0600

    sequelize added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 
+const config = require('./config/app.js')
+
 const app = express()
 
-const port = 3000
+
+app.get('/home', (req, res) => {
+    return res.send('home screen')
+})
+app.get('/login', (req, res) => {
+    return res.send('login screen')
+})
+const port = config.appPort
 
 app.listen(port, () => {
     console.log('server listening on port: ', port)
